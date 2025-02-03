78018d62f5ba304387eb7f1382770e674214a10e
tommy <l.tommy.sjoblom@gmail.com>
2025-02-03 21:35:48 +0100

    Code cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.Win32;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System.Drawing;
 using System.Globalization;
-using System.Text;
 using System.Windows;
 
 namespace TrayWeek
@@ -31,22 +28,19 @@ namespace TrayWeek
             var fgColor = Color.Black;
             var bgColor = Color.White;
 
-            Bitmap bitmap;
             var bitmapSize = 16;
             var fontSize = 8;
 
-            Font font = new Font("Segoe UI", fontSize, System.Drawing.FontStyle.Regular);
-            bitmap = new Bitmap(bitmapSize, bitmapSize);
+            var font = new Font("Segoe UI", fontSize, System.Drawing.FontStyle.Regular);
+            var bitmap = new Bitmap(bitmapSize, bitmapSize);
+
             Graphics graphics = Graphics.FromImage(bitmap);
             var stringSize = graphics.MeasureString(input, font, 256);
             graphics.FillRectangle(new SolidBrush(bgColor), 0, 0, bitmapSize - 1, bitmapSize - 1);
             graphics.DrawRectangle(new Pen(fgColor), 0, 0, bitmapSize - 1, bitmapSize - 1);
             graphics.DrawString(input, font, new SolidBrush(fgColor), bitmapSize / 2 - stringSize.Width / 2, bitmapSize / 2 - stringSize.Height / 2 + 1);
 
-            var iconHandle = bitmap.GetHicon();
-            var icon = System.Drawing.Icon.FromHandle(iconHandle);
-
-            return icon;
+            return System.Drawing.Icon.FromHandle(bitmap.GetHicon());
         }
 
         private void ContextMenuItemExit_Click(object sender, RoutedEventArgs e)
