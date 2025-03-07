8f9eae9c3c8995b8e67cc60a616c9b1a017ba894
tommy <l.tommy.sjoblom@gmail.com>
2025-03-07 20:49:20 +0100

    Better placement if week starts with 1, i.e. between 10 and 19

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,12 +33,14 @@ namespace TrayWeek
 
             var font = new Font("Segoe UI", fontSize, System.Drawing.FontStyle.Regular);
             var bitmap = new Bitmap(bitmapSize, bitmapSize);
+            var extraOffset = input.Length == 2 && input.Substring(0, 1) == "1" ? -1 : 0;
 
             var graphics = Graphics.FromImage(bitmap);
             var stringSize = graphics.MeasureString(input, font, 256);
+
             graphics.FillRectangle(new SolidBrush(bgColor), 0, 0, bitmapSize - 1, bitmapSize - 1);
             graphics.DrawRectangle(new Pen(fgColor), 0, 0, bitmapSize - 1, bitmapSize - 1);
-            graphics.DrawString(input, font, new SolidBrush(fgColor), bitmapSize / 2 - stringSize.Width / 2, bitmapSize / 2 - stringSize.Height / 2 + 1);
+            graphics.DrawString(input, font, new SolidBrush(fgColor), bitmapSize / 2 - stringSize.Width / 2 + extraOffset, bitmapSize / 2 - stringSize.Height / 2 + 1);
 
             return System.Drawing.Icon.FromHandle(bitmap.GetHicon());
         }
